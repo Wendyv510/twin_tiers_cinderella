597252915aa37e7eedd80de58f8dd8b9b5e39008
wendyv510 <wendyv510@hotmail.com>
2020-04-09 16:17:50 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-   root 'application#home' 
+  root 'application#home' 
 
+  get '/login' => "sessions#new" 
   get '/auth/facebook/callback' => 'sessions#create' 
+  post '/login' => "sessions#create" 
+  post '/logout' => "sessions#destroy" 
+
+  
 
 end
