771ce9aa3385f1b4e0f642428d746d7b893bfa63
wendyv510 <wendyv510@hotmail.com>
2020-04-09 17:28:01 -0400

    team model

---
+++ app/models/team.rb
@@ -0,0 +1,44 @@
+class Team < ApplicationRecord
+    belongs_to :user 
+    has_many :players 
+
+    def index 
+        @teams = Team.all 
+    end 
+
+    def new 
+        @team = Team.new 
+    end 
+
+    def create 
+        @team = Team.create(team_params)
+           redirect_to team_path(@team)  
+    end 
+
+    def show 
+        @team = Team.find(params[:id]) 
+    end 
+
+    def edit 
+        @team = Team.find(params[:id]) 
+    end 
+
+    def update 
+        @team = Team.find(params[:id]) 
+        @team.update(name: params[:town], color: params[:color], age_group params[:age_group]) 
+           redirect_to team_path(@team) 
+    end 
+
+    def delete 
+        @team = Team.find(params[:id]) 
+        @team.delete 
+           redirect_to root_path 
+    end 
+
+
+       private 
+
+    def team_params 
+        params.require(:team).permit(:town, :color, :age_group) 
+    end 
+end
