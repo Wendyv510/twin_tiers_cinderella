b4f96cab5c81e74c090bbab22ff0a8eb5eabcf40
wendyv510 <wendyv510@hotmail.com>
2020-04-12 15:47:51 -0400

    validations

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,7 @@
 class Player < ApplicationRecord
      belongs_to :coach 
      belongs_to :team 
-     
+
+     validates :name, :presence => true 
+     validates :age, :presence => true 
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,7 @@ class Team < ApplicationRecord
     belongs_to :user 
     has_many :players 
 
-    
+    validates :town, :presence => true 
+    validates :color, presence => true 
+    validates :age_group, :presence => true  
 end
