a89a185de7216db188acb8288d84d01f2ea89d82
wendyv510 <wendyv510@hotmail.com>
2020-04-13 13:56:54 -0400

    authenticity token/partial with new/login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,20 @@
 class SessionsController < ApplicationController
 
     def new 
-        @user = User.new 
-          redirect_to '/login'  
+        @user = User.new
+        render 'users/login'    
     end 
 
     def create 
         if auth = request.env["omniauth.auth"]
            @user = User.find_or_create_by_omniauth(auth) 
            session[:user_id] = @user.id 
-           redirect_to 'users/show'  
+           render 'users/login'  
         else
            @user = User.find_by(name:params[:name])
               if @user && @user.authenticate(params[:password])  
               session[:user_id] = @user.id 
-            redirect_to 'users/show'  
-        else 
-            render '/new' 
+            render 'users/login'  
         end 
         end 
     end 
