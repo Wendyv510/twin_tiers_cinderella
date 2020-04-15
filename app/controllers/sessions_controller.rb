ad5b34e46435134b710994977882f11aaf6d05c1
wendyv510 <wendyv510@hotmail.com>
2020-04-15 09:54:57 -0400

    omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -22,7 +22,7 @@ class SessionsController < ApplicationController
         @user = User.find_or_create_by(uid: auth['uid']) do |u|
             u.name = auth['info']['name']
             u.email = auth['info']['email']
-            u.image = auth['info']['image']
+            u.password = auth['uid']
           end
        
           session[:user_id] = @user.id
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-    provider :facebook, ENV['253033765735907'], ENV['c7a751e652582a03f7e11b28c69eece0']
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
