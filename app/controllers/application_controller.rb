4f62945943ec49703ea635ea69803004696c5291
wendyv510 <wendyv510@hotmail.com>
2020-04-16 14:26:22 -0400

    homepage navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
 
     def authentication_required 
-        if !logged_in 
+        if !logged_in? 
             redirect_to login_path 
         end 
     end 
