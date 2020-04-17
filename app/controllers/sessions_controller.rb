3220babff4dc60c962832328df5d35901389a22d
wendyv510 <wendyv510@hotmail.com>
2020-04-17 14:40:41 -0400

    debugging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,8 +13,8 @@ class SessionsController < ApplicationController
     end 
 
     def create 
-        @user = User.find_by(name:params[:user][:name])
-          if @user && @user.authenticate(params[:user][:password])  
+        @user = User.find_by(email: params[:email])
+          if @user && @user.authenticate(params[:password])  
             session[:user_id] = @user.id 
             redirect_to users_path(@user) 
           else 
