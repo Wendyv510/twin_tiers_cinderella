b7e9bda16b0b34657040b999efc771c14507b33d
wendyv510 <wendyv510@hotmail.com>
2020-04-17 17:51:57 -0400

    seeds file

---
+++ db/players_seeds.rb
@@ -0,0 +1,17 @@
+after :teams do 
+    @team = Team_find_by(@user_id) 
+    Team.players.create(
+        name: Shelly Mills, 
+        age: 8,
+        user_id: @user.id,
+        team_id: @team.id
+     ) 
+     
+     Team.players.create(
+        name: Hillary Sip, 
+        age: 8,
+        user_id: @user.id,
+        team_id: @team.id 
+     )    
+
+     
