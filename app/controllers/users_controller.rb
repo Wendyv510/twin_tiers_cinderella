b0cae4fc04670508ba2e13a348c807e084e9da67
Wendy Harris <wendyv510@hotmail.com>
2020-04-25 14:46:06 -0400

    misc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
         if @user.save 
            redirect_to login_path  
         else 
-           render users_new_path  
+           render :new  
         end 
     end 
 
